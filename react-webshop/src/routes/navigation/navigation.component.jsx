97d0e24e77a4455166243dd68363754804a95368
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-04-04 11:42:42 +0200

    Ipmlemented BrowserRouter / Routes/Links/Navbar

---
+++ react-webshop/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import {ReactComponent as BeRealLogo} from '../../assets/be-real-perfect-svg_757064-35.svg'
+import './navigation.style.scss';
+
+export default function Navigation() {
+  return (
+    <>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+            <BeRealLogo className='logo'/>
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+}
