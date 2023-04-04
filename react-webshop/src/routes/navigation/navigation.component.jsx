49272fe1bf6dc699509c400af462b1e62dbe9771
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-04-04 18:56:47 +0200

    Firbase  authentification, signIn component + reg

--- react-webshop/src/routes/navigation/navigation.component.jsx
+++ react-webshop/src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import {ReactComponent as BeRealLogo} from '../../assets/be-real-perfect-svg_757064-35.svg'
+import { ReactComponent as BeRealLogo } from '../../assets/be-real-perfect-svg_757064-35.svg';
 import './navigation.style.scss';
 
 export default function Navigation() {
@@ -8,11 +8,14 @@ export default function Navigation() {
     <>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-            <BeRealLogo className='logo'/>
+          <BeRealLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
             SHOP
+          </Link>
+          <Link className='nav-link' to='/signIn'>
+            Sign In
           </Link>
         </div>
       </div>
