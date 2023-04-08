0567bca365418ed2eda272cd290bca2a36519ca9
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-04-09 00:52:49 +0200

    mobile dropdown position fixed. UX subtitle added

--- react-webshop/src/components/category-preview.component.jsx/category-preview.component.jsx
+++ react-webshop/src/components/category-preview.component.jsx/category-preview.component.jsx
@@ -3,20 +3,25 @@ import { Link } from 'react-router-dom';
 import ProducCard from '../product-card/product-card.component';
 import './category-preview.styles.scss';
 
-export default function CategoryPreview({ category , products }) {
+export default function CategoryPreview({ category, products }) {
     return (
         <div className="category-preview-container">
             <h2>
                 <Link className='title' to={`/shop/${category.toLowerCase()}`}>
-                {category.toUpperCase()}
-               </Link>
+                    {`${category.toUpperCase()} COLLECTION `}
+                </Link>
             </h2>
+            <div className='category-preview-subtitle'>
+                <Link className='category-preview-subtitle' to={`/shop/${category.toLowerCase()}`}>
+                    {`click to explore more ...`}
+                </Link>
+            </div>
             <div className="preview">
                 {
                     products
-                    .filter((_, idx)=> idx < 4)
-                    .map((product)=>
-                    <ProducCard key={product.id} product={product}/> )
+                        .filter((_, idx) => idx < 4)
+                        .map((product) =>
+                            <ProducCard key={product.id} product={product} />)
                 }
             </div>
         </div>
