6e892fb426258dac175cd6ce73ff2bee5bb91085
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2023-05-15 13:15:47 +0200

    Stripe credit card payment integrated

--- react-webshop/src/index.js
+++ react-webshop/src/index.js
@@ -6,18 +6,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Elements } from '@stripe/react-stripe-js';
-
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
+import { Elements } from '@stripe/react-stripe-js';
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading = {null} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-         <Elements stripe = {stripePromise}>
+          <Elements stripe={stripePromise}>
             <App />
           </Elements>
         </BrowserRouter>
--- react-webshop/src/store/root-reducer.js
+++ react-webshop/src/store/root-reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { categoriesReducer } from './categories/categories.reducer';
 import { cartReducer } from './cart/cart.reducer';
-
 import { userReducer } from './user/user.reducer';
 
 export const rootReducer = combineReducers({
